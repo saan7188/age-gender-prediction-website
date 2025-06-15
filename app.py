9cb9c3930241e6cd0bb3b028c6de18030925f630
saan7188 <ssksanjay66@gmail.com>
2025-06-15 13:36:04 +0530

    port

--- app.py
+++ app.py
@@ -132,4 +132,5 @@ def report_error():
     return redirect(url_for('index', lang=lang))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
